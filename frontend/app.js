ea6fba71c278200036a0f20c8c89fc367399074a
Jeremy Brown <jj.brown@hotmail.com>
2017-03-30 15:51:26 -0400

    Initial barebones page with departure/destination fields + map & direction placeholders

---
+++ frontend/app.js
@@ -0,0 +1,31 @@
+// Roadtrip planner app
+// Main functions
+
+function fetchDirections(departure, arrival) {
+    // TODO
+    console.log('Fetching directions')
+}
+
+function fetchRestaurants(departure, arrival) {
+    // TODO
+    console.log('Fetching restaurants')
+}
+
+function fetchGasStations(departure, arrival) {
+    // TODO
+    console.log('Fetching gas stations')
+}
+
+function fetchWeather(departure, arrival) {
+    // TODO
+    console.log('Fetching weather')
+}
+
+function fetchTripInfo(departure, arrival) {
+    console.log('Fetching trip info')
+    fetchDirections(departure, arrival)
+    //fetchRestaurants(departure, arrival)
+    //fetchGasStations(departure, arrival)
+    //fetchWeather(departure, arrival)
+}
+
